0d1d963b1acc8f165013d12e3bab2681d187572f
Trendafil Dabov <trendafil@github.com>
2021-06-10 19:58:16 +0300

    Refactor frontend - butttons

--- client/src/scripts/main.js
+++ client/src/scripts/main.js
@@ -41,5 +41,43 @@ function show() {
     }
 
     counter++;
+};
 
-}
+//statuses
+const btn5 = document.getElementById('status5');
+const btn4 = document.getElementById('status4');
+const btn3 = document.getElementById('status3');
+const btn2 = document.getElementById('status2');
+const btn1 = document.getElementById('status1');
+
+btn5.addEventListener('click', function() {
+    if (btn4.style.marginTop != '290px') {
+        btn4.style.marginTop = '290px';
+    } else {
+        btn4.style.marginTop = '20px';
+    }
+});
+
+btn4.addEventListener('click', function() {
+    if (btn3.style.marginTop != '290px') {
+        btn3.style.marginTop = '290px';
+    } else {
+        btn3.style.marginTop = '20px';
+    }
+});
+
+btn3.addEventListener('click', function() {
+    if (btn2.style.marginTop != '290px') {
+        btn2.style.marginTop = '290px';
+    } else {
+        btn2.style.marginTop = '20px';
+    }
+});
+
+btn2.addEventListener('click', function() {
+    if (btn1.style.marginTop != '290px') {
+        btn1.style.marginTop = '290px';
+    } else {
+        btn1.style.marginTop = '20px';
+    }
+});
